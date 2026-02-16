2d5b153001e513bf789380206bef6c3e2a257cdf
winggar <winggar1228@gmail.com>
2026-02-16 13:20:33 -0800

    rewrite geology to be much simpler

--- planet/src/main/kotlin/dev/biserman/planet/planet/WorldKinds.kt
+++ planet/src/main/kotlin/dev/biserman/planet/planet/WorldKinds.kt
@@ -2,7 +2,10 @@ package dev.biserman.planet.planet
 
 import dev.biserman.planet.planet.tectonics.StonePlacement
 import dev.biserman.planet.planet.tectonics.StonePlacementType
+import dev.biserman.planet.things.Stone
 
 class WorldKinds {
-    val stonePlacements = mapOf<StonePlacementType, StonePlacement>()
+    var stonePlacements = mapOf<StonePlacementType, StonePlacement>()
+    lateinit var defaultSurfaceStone: Stone
+    lateinit var defaultDeepStone: Stone
 }
