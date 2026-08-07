6d9b4b41bdaed100cd1ed7a5735f94c48d645a69
winggar <winggar1228@gmail.com>
2026-08-07 02:23:36 -0700

    remove incorrect license from class

--- planet/src/main/kotlin/dev/biserman/planet/Rotate.kt
+++ planet/src/main/kotlin/dev/biserman/planet/Rotate.kt
@@ -4,15 +4,6 @@ import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.api.MeshInstance3D
 
-// Adapted from Andy Gainey, original license below:
-// Copyright © 2014 Andy Gainey <andy@experilous.com>
-//
-// Usage of the works is permitted provided that this instrument
-// is retained with the works, so that any entity that uses the
-// works is notified of this instrument.
-//
-// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
-
 @RegisterClass
 class Rotate : MeshInstance3D() {
     @RegisterFunction
